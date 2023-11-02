efefbf057467697dda837ef1c6d958381552edd3
Kriiiiss <jic128@ucsd.edu>
2023-11-01 22:43:21 -0700

    Update TestDocSearch.java

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -18,5 +18,14 @@ public class TestDocSearch {
     String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
     assertEquals(expect, h.handleRequest(rootPath));
 	}
+
+    @Test
+    public void testgrep() throws URISyntaxException, IOException {
+    Handler h = new Handler("./technical/");
+    URI rootPath = new URI("http://localhost/search?q=Resonance");
+    String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
+
+    assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
+	}
 }
 
